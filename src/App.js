36afee286811093d41a93ca76411878e1370a978
Douglas Strey <douglas.morini@hotmail.com>
2021-09-11 15:42:59 -0300

    Estilização básica no MovieRow e implementação no App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import './App.css';
 
 import Tmdb from './Tmdb';
-import MovieRow from './components/MovieRow';
+import MovieRow from './components/MovieRow/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie/FeaturedMovie';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState([null]);
 
   useEffect(()=>{
     const loadAll = async () => {
@@ -20,9 +23,14 @@ export default () => {
 
   return (
     <div className="page">
+
+        {featuredData &&
+            <FeaturedMovie item={featuredData} />
+        }
+
       <section className="list">
         { movieList.map((item, key) => (
-          <MovieRow key={key} />
+          <MovieRow key={key} title={item.title} items={item.items}/>
         )) }
       </section>
     </div>
